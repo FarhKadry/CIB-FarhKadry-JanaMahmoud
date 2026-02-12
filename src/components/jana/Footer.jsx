c6bc41b078e1567b6b86865141e8678f995a6769
Jana <23-101109@students.eui.edu.eg>
2026-02-12 20:54:27 +0200

    starting footer component

--- src/components/jana/Footer.jsx
+++ src/components/jana/Footer.jsx
@@ -1,10 +1,28 @@
 import React from 'react';
+import footerdeco1 from '../../assets/footerdeco2.png';
+import footerdeco2 from '../../assets/footerdeco.svg';
+import logo from '../../assets/logo.svg';
+import './Footer.css';
 
 const Footer = () => {
     return ( 
         <>
         <footer>
-            
+            <div className="footerbg">
+                <div className="footerBgTop">
+                    <img src={footerdeco1} alt="decor" />
+                </div>
+            <div className="footerCont">
+                <div className="footerContTop">
+                    <img src={logo} alt="CIB logo" />
+                    <div className="footeritems">
+                    </div>
+                </div>
+            </div>
+                <div className="footerBgBottom">
+                    <img src={footerdeco2} alt="decor" />
+                </div>
+            </div>
         </footer>
         </>
      );
