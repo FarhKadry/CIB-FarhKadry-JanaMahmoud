73a5873acf5110da64888866e635b29c9c636e8a
Jana <23-101109@students.eui.edu.eg>
2026-02-12 21:18:51 +0200

    finalizing footer

--- src/components/jana/Footer.jsx
+++ src/components/jana/Footer.jsx
@@ -4,6 +4,16 @@ import footerdeco2 from '../../assets/footerdeco.svg';
 import logo from '../../assets/logo.svg';
 import './Footer.css';
 import FooterItem from './FooterItem';
+import store1 from '../../assets/store01.svg';
+import store2 from '../../assets/store02.svg';
+import facebook from '../../assets/socials01.svg';
+import instagram from '../../assets/socials02.svg';
+import linkedin from '../../assets/socials03.svg';
+import youtube from '../../assets/socials04.svg';
+import twitter from '../../assets/socials05.svg';
+import whatsapp from '../../assets/socials06.svg';
+
+
 
 const Footer = () => {
     return ( 
@@ -35,6 +45,38 @@ const Footer = () => {
                             <FooterItem />
                             <FooterItem />
                         </div>
+                    </div>
+                    <div className="footerApps">
+                        <img src={store1} alt="get it on google play icon" />
+                        <img src={store2} alt="download on app store icon" />
+                    </div>
+                </div>
+                <div className="footerContBottom">
+                    <div className="footerItemsBottom">
+                        <FooterItem />
+                        <FooterItem />
+                        <FooterItem />
+                        <FooterItem />
+                    </div>
+                    <div className="footerSocials">
+                        <a href="#home">
+                            <img src={facebook} alt="facebook icon" />
+                        </a>
+                        <a href="#home">
+                            <img src={instagram} alt="instagram icon" />
+                        </a>
+                        <a href="#home">
+                            <img src={linkedin} alt="linkedin icon" />
+                        </a>
+                        <a href="#home">
+                            <img src={youtube} alt="youtube icon" />
+                        </a>
+                        <a href="#home">
+                            <img src={twitter} alt="twitter icon" />
+                        </a>
+                        <a href="#home">
+                            <img src={whatsapp} alt="whatsapp icon" />
+                        </a>
                     </div>
                 </div>
             </div>
