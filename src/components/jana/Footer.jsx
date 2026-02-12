b2ce28f5f63150160d18727b2c71df2d95f56788
Jana <23-101109@students.eui.edu.eg>
2026-02-12 21:01:54 +0200

    footer item component

--- src/components/jana/Footer.jsx
+++ src/components/jana/Footer.jsx
@@ -3,6 +3,7 @@ import footerdeco1 from '../../assets/footerdeco2.png';
 import footerdeco2 from '../../assets/footerdeco.svg';
 import logo from '../../assets/logo.svg';
 import './Footer.css';
+import FooterItem from './FooterItem';
 
 const Footer = () => {
     return ( 
@@ -16,6 +17,24 @@ const Footer = () => {
                 <div className="footerContTop">
                     <img src={logo} alt="CIB logo" />
                     <div className="footeritems">
+                        <div className="footerItemsCol">
+                            <FooterItem />
+                            <FooterItem />
+                            <FooterItem />
+                            <FooterItem />
+                        </div>
+                        <div className="footerItemsCol">
+                            <FooterItem />
+                            <FooterItem />
+                            <FooterItem />
+                            <FooterItem />
+                        </div>
+                        <div className="footerItemsCol">
+                            <FooterItem />
+                            <FooterItem />
+                            <FooterItem />
+                            <FooterItem />
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ src/components/jana/FooterItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './FooterItem.css';
+
+const FooterItem = () => {
+    return ( 
+        <h5 className='footeritem'>FAQs</h5>
+     );
+}
+ 
+export default FooterItem;
